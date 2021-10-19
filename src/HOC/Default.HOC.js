52f0352dac0c065e3edcda753896837937964014
smritisrutu9 <smritishruti9@gmail.com>
2021-10-19 22:45:13 +0530

    first commit

---
+++ src/HOC/Default.HOC.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import DefaultLayout from "../layouts/Default.layout";
+const DefaultHOC= ({ component:Component , ...rest}) =>{
+    return (
+        <>
+        <Route {...rest} component={(props)=>(
+            <DefaultLayout>
+                <Component {...props}/>
+
+
+            </DefaultLayout>
+        )
+        }></Route>
+         
+        </>
+    )
+};
+export default DefaultHOC;
---
+++ src/planning.js
@@ -0,0 +1,18 @@
+//Book My show planning
+// We need a reusable carousal
+// Hero CArousal
+// Common carousal
+// cast carousal (small carousal)
+
+// pages:
+// Home page
+// CAtegory page
+// Auth page
+// Razorpay Payment gateway
+
+// Plan how we are Building it?
+
+// Layout
+// Routes
+// Pages
+// Components
